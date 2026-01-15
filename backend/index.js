14a847b48c653e70d43a49d145206ab7f95c5476
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2026-01-15 12:04:54 -0300

    feat: conexão com o mysql

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 import express from 'express'
+import mysql from 'mysql'
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+})
 
 app.listen(8800, () => {
     console.log('conectado ao back-end!')
